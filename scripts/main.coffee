aca0f5b09093dc28023c276a7185688d5df74704
CT <draziwfozo@gmail.com>
2017-09-10 14:57:29 -0400

    Times style works

--- scripts/main.coffee
+++ scripts/main.coffee
@@ -1,16 +1,38 @@
 classes = [
-  1, 2, 3, 4,
-  2, 3, 4, 1,
-  3, 4, 1, 2,
-  5, 6, 7, 8,
-  5, 6, 7, 8,
-  5, 6, 7, 8,
-  8, 7, 6, 5,
-  7, 8, 5, 6
+  [1, 2, 3, 5, 5, 5, 6, 7], #A
+  [2, 3, 4, 6, 6, 6, 7, 8], #B
+  [3, 4, 1, 7, 7, 7, 6, 5], #C
+  [5, 6, 7, 8, 8, 8, 5, 6]  #D
 ]
 
+rooms = [
+  "",     #1
+  "2057", #2
+  "460",  #3
+  "2046", #4
+  "3033", #5
+  "2001", #6
+  "3038", #7
+  "2058"  #8
+]
+
+cell =
+  (letter, period) ->
+    (index, html) ->
+      "<div class=\"period\">
+        <div class=\"top\">
+          <span class=\"time\">7:30 - 8:20</span>
+          <span class=\"room\">3033</span>
+        </div>
+        <h2 class=\"text-center\">#{classes[letter][period]}</h2>
+      </div>"
+
+
+
 main = () ->
-  cells = $( "div.period" )
-  cells.eq(i).html(classes[i]) for i in [0..27]
+  day = $( "div.day" ).first()
+  for letter in [0..3]
+    day.append(cell(letter, period)) for period in [0..7]
+    day = day.next()
 
 $(document).ready(main)
--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,24 @@
-var classes, main;
+var cell, classes, main, rooms;
 
-classes = [1, 2, 3, 4, 2, 3, 4, 1, 3, 4, 1, 2, 5, 6, 7, 8, 5, 6, 7, 8, 5, 6, 7, 8, 8, 7, 6, 5, 7, 8, 5, 6];
+classes = [[1, 2, 3, 5, 5, 5, 6, 7], [2, 3, 4, 6, 6, 6, 7, 8], [3, 4, 1, 7, 7, 7, 6, 5], [5, 6, 7, 8, 8, 8, 5, 6]];
+
+rooms = ["", "2057", "460", "2046", "3033", "2001", "3038", "2058"];
+
+cell = function(letter, period) {
+  return function(index, html) {
+    return "<div class=\"period\"> <div class=\"top\"> <span class=\"time\">7:30 - 8:20</span> <span class=\"room\">3033</span> </div> <h2 class=\"text-center\">" + classes[letter][period] + "</h2> </div>";
+  };
+};
 
 main = function() {
-  var cells, i, j, results;
-  cells = $("div.period");
+  var day, i, j, letter, period, results;
+  day = $("div.day").first();
   results = [];
-  for (i = j = 0; j <= 27; i = ++j) {
-    results.push(cells.eq(i).html(classes[i]));
+  for (letter = i = 0; i <= 3; letter = ++i) {
+    for (period = j = 0; j <= 7; period = ++j) {
+      day.append(cell(letter, period));
+    }
+    results.push(day = day.next());
   }
   return results;
 };
