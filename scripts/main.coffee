dc1ffc3554a53036bb54fb2254367dbec8f1b528
CT <draziwfozo@gmail.com>
2017-09-10 15:06:39 -0400

    Periods and times work

--- scripts/main.coffee
+++ scripts/main.coffee
@@ -1,5 +1,5 @@
 classes = [
-  [1, 2, 3, 5, 5, 5, 6, 7], #A
+  [1, 2, 3, 5, 5, 5, 8, 7], #A
   [2, 3, 4, 6, 6, 6, 7, 8], #B
   [3, 4, 1, 7, 7, 7, 6, 5], #C
   [5, 6, 7, 8, 8, 8, 5, 6]  #D
@@ -16,15 +16,38 @@ rooms = [
   "2058"  #8
 ]
 
+startTimes = [
+  "7:30",
+  "8:25",
+  "9:50",
+  "10:45",
+  "11:20",
+  "11:55",
+  "12:30",
+  "1:25"
+]
+
+endTimes = [
+  "8:20",
+  "9:45",
+  "10:40",
+  "11:15",
+  "11:50",
+  "12:25",
+  "1:20",
+  "2:15"
+]
+
 cell =
   (letter, period) ->
     (index, html) ->
+      perNum = classes[letter][period]
       "<div class=\"period\">
         <div class=\"top\">
-          <span class=\"time\">7:30 - 8:20</span>
-          <span class=\"room\">3033</span>
+          <span class=\"time\">#{startTimes[period]} - #{endTimes[period]}</span>
+          <span class=\"room\">#{rooms[perNum-1]}</span>
         </div>
-        <h2 class=\"text-center\">#{classes[letter][period]}</h2>
+        <h2 class=\"text-center\">#{perNum}</h2>
       </div>"
 
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,18 @@
-var cell, classes, main, rooms;
+var cell, classes, endTimes, main, rooms, startTimes;
 
-classes = [[1, 2, 3, 5, 5, 5, 6, 7], [2, 3, 4, 6, 6, 6, 7, 8], [3, 4, 1, 7, 7, 7, 6, 5], [5, 6, 7, 8, 8, 8, 5, 6]];
+classes = [[1, 2, 3, 5, 5, 5, 8, 7], [2, 3, 4, 6, 6, 6, 7, 8], [3, 4, 1, 7, 7, 7, 6, 5], [5, 6, 7, 8, 8, 8, 5, 6]];
 
 rooms = ["", "2057", "460", "2046", "3033", "2001", "3038", "2058"];
 
+startTimes = ["7:30", "8:25", "9:50", "10:45", "11:20", "11:55", "12:30", "1:25"];
+
+endTimes = ["8:20", "9:45", "10:40", "11:15", "11:50", "12:25", "1:20", "2:15"];
+
 cell = function(letter, period) {
   return function(index, html) {
-    return "<div class=\"period\"> <div class=\"top\"> <span class=\"time\">7:30 - 8:20</span> <span class=\"room\">3033</span> </div> <h2 class=\"text-center\">" + classes[letter][period] + "</h2> </div>";
+    var perNum;
+    perNum = classes[letter][period];
+    return "<div class=\"period\"> <div class=\"top\"> <span class=\"time\">" + startTimes[period] + " - " + endTimes[period] + "</span> <span class=\"room\">" + rooms[perNum - 1] + "</span> </div> <h2 class=\"text-center\">" + perNum + "</h2> </div>";
   };
 };
 
